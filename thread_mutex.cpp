68eeaaebdad80603d414edf17c4d593c8447c9de
Suryanarayanan-S <suryasoundar96@gmail.com>
2018-07-27 10:20:49 +0530

    Sample programs

---
+++ thread_mutex.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<thread>
+int testMultithreadedWallet()
+{
+   Wallet walletObject;
+   std::vector<std::thread> threads;
+   for(int i = 0; i < 5; ++i){
+        threads.push_back(std::thread(&Wallet::addMoney, &walletObject, 1000));
+   }
+ 
+   for(int i = 0; i < threads.size() ; i++)
+   {
+       threads.at(i).join();
+   }
+   return walletObject.getMoney();
+}
+ 
+int main()
+{
+ 
+  int val = 0;
+  for(int k = 0; k < 1000; k++)
+  {
+     if((val = testMultithreadedWallet()) != 5000)
+     {
+       std::cout << "Error at count = "<<k<<" Money in Wallet = "<<val << std::endl;
+     }
+  }
+  retu
