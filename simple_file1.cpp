ce2a216587fc804b19073c66a9ca17f12fe8b9e1
Suryanarayanan-S <suryasoundar96@gmail.com>
2018-08-03 11:06:18 +0530

    sample File handling

---
+++ simple_file1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<fstream>
+using namespace std;
+ 
+int main() 
+{
+char data[100]; 
+ifstream ifile; 
+ifile.open ("text.txt"); 
+while ( !ifile.eof() ) 
+{
+ifile.getline (data, 100); 
+cout << data << endl; 
+}
+ifile.close();
+return 0;
+}
